4de1c024c7d63a9e0c288392e2b95812e6ecebc4
Erick Goldberg <erick_goldberg@hotmail.com>
2024-09-26 06:08:07 -0300

    Base API Gateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseOcelot().Wait();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
